be45dd8e853fc6af040ad92c8b604bb607908657
Christina Leung <tinalina1003@gmail.com>
2023-09-05 15:42:13 -0400

    Updating code without pandas... currently only have total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,53 @@
+import os
+import csv
+
+# import csv data from Resources file
+pypollCSV = os.path.join("Resources", "election_data.csv")
+
+##################################
+# initialize lists and variables #
+##################################
+
+candidate = set() # create a hashset
+totalVotes = 0
+candidateVote = 0
+listOfCandidates = []
+ballot = {"Candidate": "", "Votes": ""}
+
+
+
+
+##############################
+# extract data from csv file #
+##############################
+
+with open(pypollCSV) as csvfile:
+
+    # cleans up the csv
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    csvHeader = next(csvreader)
+
+
+    for row in csvreader:
+
+        candidate.add(row[2])
+    
+        # counts total votes
+        totalVotes += 1
+
+
+
+
+print(totalVotes)
+
+
+
+
+
+
+
+"""
 import pandas as pd
 from pathlib import Path
 
@@ -24,3 +74,4 @@ winner = candidate_counts.idxmax()
 print(f"\nThe winner is {winner}")
 
 print(df)
+"""
